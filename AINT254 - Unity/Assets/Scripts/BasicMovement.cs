2a0ce531769d22e76010596c0946a834246b4d05
Nick1441 <43572914+Nick1441@users.noreply.github.com>
2019-10-03 13:01:35 +0100

    Created Project Files
    Unity
     - Created Unity Folder For Game.
     - Began to create Player And Began
       Looking into the Movement Mechanic.
    - Created Movement Script (Needs
      Work).
    - Added Git ignore.

---
+++ AINT254 - Unity/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+       
+        //transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+
+    public void Move()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Test");
+            Vector3 MoveVec = this.transform.position;
+            MoveVec.z = MoveVec.z + 1;
+            this.transform.position = MoveVec;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 MoveVec = this.transform.position;
+            MoveVec.x = MoveVec.x - 1;
+            this.transform.position = MoveVec;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Vector3 MoveVec = this.transform.position;
+            MoveVec.x = MoveVec.x += 1;
+            this.transform.position = MoveVec;
+        }
+    }
+}
