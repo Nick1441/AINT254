bf4f99e577416c4a654a7bc5acd0563133f2733f
Nick1441 <43572914+Nick1441@users.noreply.github.com>
2019-10-14 13:06:05 +0100

    Weapon Sprint
    - Created Weapon For player
    - Created Bullet Prefab and Attacked to Weapon.
    - Created Cool Down For Weapon
    - Created Spawn Point for Weapon

---
+++ AINT254 - Unity/Assets/AmmoScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoScript : MonoBehaviour
+{
+    public float MoveSpeed = 100.0f;
+    public int Damage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * MoveSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AINT254 - Unity/Assets/Scripts/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public GameObject Ammo;
+    public Transform[] BulletSpawn;
+    public float FireCoolDown = 1.0f;
+    private bool isFiring = false;
+
+
+
+    public void Fire()
+    {
+        isFiring = true;
+        for (int i = 0; i < BulletSpawn.Length; i++)
+        {
+            Instantiate(Ammo, BulletSpawn[i].position, BulletSpawn[i].rotation);
+        }
+        Invoke("SetFiring", FireCoolDown);
+    }
+
+    public void SetFiring()
+    {
+        isFiring = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!isFiring)
+            {
+                Fire();
+            }
+        }
+    }
+}
