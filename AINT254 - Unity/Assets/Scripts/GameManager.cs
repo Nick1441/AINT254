d9699560beb6a4e14bda3c8bfb34c9b43f4fe52e
Nick1441 <43572914+Nick1441@users.noreply.github.com>
2019-11-09 13:56:28 +0000

    Created Scenes/Death Animation
    Created Scenes with Working Buttons to Navigate Through levels.
    created basic Animation When player Dies, To be Developed Further. Helps Create Better User Experience.

---
+++ AINT254 - Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //GAME
+    public void EndGame()
+    {
+        SceneManager.LoadScene("EndScene");
+    }
+
+
+
+    //MAIN MENU
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+    //END MENU
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
