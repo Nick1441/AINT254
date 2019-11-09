4d5b2d542ba0e915fd76b437413e4b432df659c2
Nick1441 <43572914+Nick1441@users.noreply.github.com>
2019-11-09 14:36:02 +0000

    Started Creating Enemy
    Created Look At Target Script
    Created Enenmy Weapon
    Created Enemy Shooting at Player Script (Random Time in Range - Chanagable)
    Created Enemy Ammo
    Created Rnages which can be edited, to allow overall Challenge Difficulty Changes

---
+++ AINT254 - Unity/Assets/Scripts/EnemyWeapon.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeapon : MonoBehaviour
+{
+
+    public GameObject Ammo;
+    public Transform[] BulletSpawn;
+    private bool Dead = false;
+    public float LowRange = 1f;
+    public float HighRange = 5f;
+
+
+
+    public void Fire()
+    {
+        for (int i = 0; i < BulletSpawn.Length; i++)
+        {
+            Instantiate(Ammo, BulletSpawn[i].position, BulletSpawn[i].rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        RandomShooting();
+    }
+
+    public void RandomShooting()
+    {
+        if (Dead == false)
+        {
+            float Rand = Random.Range(LowRange, HighRange);
+            Invoke("FireMeth", Rand);
+        }
+    }
+
+    public void FireMeth()
+    {
+        Fire();
+        RandomShooting();
+    }
+}
