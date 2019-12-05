0dd0dc82e85b1ef8799bd90307b51af3842f3519
Nick1441 <43572914+Nick1441@users.noreply.github.com>
2019-12-05 13:55:37 +0000

    Fixed/Created Prefabs
    Created More Challenge Prfabs To Be Spawned.
    Created More Spawner Chance Options.
    Created Script To Delete old challenges based on distance to reduce lag.

---
+++ AINT254 - Unity/Assets/Scripts/DestroyOnDistance.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnDistance : MonoBehaviour
+{
+    //Player Distance
+    public GameObject Player;
+    public float Distance;
+    public float Range = 10;
+
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player = GameObject.FindWithTag("Player");
+        Distance = Vector3.Distance(Player.transform.position, transform.position);
+
+        if (Distance >= Range)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- AINT254 - Unity/Assets/Scripts/SpawnerSpawner.cs
+++ AINT254 - Unity/Assets/Scripts/SpawnerSpawner.cs
@@ -10,7 +10,17 @@ public class SpawnerSpawner : MonoBehaviour
     public float Range = 10;
 
     //CHALLENGES
-    public GameObject LavaJump;
+    public GameObject LavaJumpSingleL;
+    public GameObject LavaJumpSingleR;
+
+    public GameObject LavaJumpDoubL;
+    public GameObject LavaJumpDoubR;
+
+    public GameObject LavaJumpTripL;
+    public GameObject LavaJumpTripR;
+
+    public GameObject EnemyAttack;
+
     public GameObject TrainDodge;
     public GameObject Spawner;
 
@@ -32,13 +42,49 @@ public class SpawnerSpawner : MonoBehaviour
         {
             Chance = Random.Range(0, 100);
 
-            //LAVA JUMP
-            if (Chance < 50)
+            //Jump L Single
+            if (Chance <= 10)
             {
-                GameObject Challenge1 = Instantiate(LavaJump, transform.position, transform.rotation) as GameObject;
+                GameObject Challenge1 = Instantiate(LavaJumpSingleL, transform.position, transform.rotation) as GameObject;
                 NextSpawnerDis = 2;
             }
-            //TRAIN HIT
+            //Jump R Single
+            else if (Chance > 10 && Chance <= 20)
+            {
+                GameObject Challenge1 = Instantiate(LavaJumpSingleR, transform.position, transform.rotation) as GameObject;
+                NextSpawnerDis = 2;
+            }
+            //Jump L Double
+            else if (Chance > 20 && Chance <= 30)
+            {
+                GameObject Challenge1 = Instantiate(LavaJumpDoubL, transform.position, transform.rotation) as GameObject;
+                NextSpawnerDis = 3;
+            }
+            //Jump R Double
+            else if (Chance > 30 && Chance <= 40)
+            {
+                GameObject Challenge1 = Instantiate(LavaJumpDoubR, transform.position, transform.rotation) as GameObject;
+                NextSpawnerDis = 3;
+            }
+            //Jump L Triple
+            else if (Chance > 40 && Chance <= 50)
+            {
+                GameObject Challenge1 = Instantiate(LavaJumpTripL, transform.position, transform.rotation) as GameObject;
+                NextSpawnerDis = 4;
+            }
+            //Jump R Triple
+            else if (Chance > 50 && Chance <= 60)
+            {
+                GameObject Challenge1 = Instantiate(LavaJumpTripR, transform.position, transform.rotation) as GameObject;
+                NextSpawnerDis = 4;
+            }
+            //Enemy Spawn
+            else if (Chance > 60 && Chance <= 80)
+            {
+                GameObject Challenge1 = Instantiate(EnemyAttack, transform.position, transform.rotation) as GameObject;
+                NextSpawnerDis = 2;
+            }
+            //Train Spawn
             else
             {
                 GameObject Challenge2 = Instantiate(TrainDodge, transform.position, transform.rotation) as GameObject;
@@ -48,26 +94,8 @@ public class SpawnerSpawner : MonoBehaviour
             Vector3 NextSpawner = new Vector3(transform.position.x, transform.position.y, transform.position.z + NextSpawnerDis);
             GameObject NextSpawn = Instantiate(Spawner, NextSpawner, transform.rotation) as GameObject;
 
-            Destroy(this);
+            Destroy(this.gameObject);
         }
-
-
-
-
-
-        //Chance = Random.Range(0, 100);
-
-        //IF LAVA JUMP Move NExt Spawner 2 Down Then Redo This. Remake Spawner Ever 2 Spots
-        //Make Tripple Layer Lava Jumpers Etc.
-
-
-        //LAVA JUMP - Single
-        //GameObject Challenge = Instantiate(LavaJump, transform.position, transform.rotation) as GameObject;
-        //Vector3 NextSpawner = new Vector3(transform.position.x, transform.position.y, transform.position.z + 2);
-        //GameObject NextSpawn = Instantiate(Spawner, NextSpawner, transform.rotation) as GameObject;
-
-        //DESTROY ITSELF
-
     }
 
 
